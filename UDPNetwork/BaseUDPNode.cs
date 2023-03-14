2abc5e582399b0b73f45b62a9be79fd00d21810c
Timur Meyliev <Timka654work@gmail.com>
2023-03-14 08:00:13 +0200

    wip

--- UDPNetwork/BaseUDPNode.cs
+++ UDPNetwork/BaseUDPNode.cs
@@ -89,11 +89,7 @@ public class BaseUDPNode
 
                     var nid = data.ReadGuid(); // from node
 
-                    var len = (int)(data.Lenght - data.Position);
-
-                    var packet = new InputPacketBuffer(data.Read(len));
-
-                    transportHandle(nid, packet);
+                    transportHandle(nid, data);
                 });
             })
             .Build();
